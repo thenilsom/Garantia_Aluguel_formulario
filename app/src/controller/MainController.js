fe47569ba98fbf1482f7d2c8babc28fd6c6915d9
55629 <55629@DESKTOP-4THO7TC>
2020-05-16 13:29:39 -0300

    .

--- app/src/controller/MainController.js
+++ app/src/controller/MainController.js
@@ -12,7 +12,7 @@
         var codSeguradora = $("input[name='codigo_seguradora']").val();
         
         
-        if(!$scope.paramUrl || (!$scope.paramUrl.var1 && !codRegistro)){
+        if(codSeguradora && (!$scope.paramUrl || !$scope.paramUrl.var1)){
         	service.alertarErro('Favor entrar em contato com a Imobiliária ou a corretora de seguros para liberação do formulário de análise.');
         	
         }else{
