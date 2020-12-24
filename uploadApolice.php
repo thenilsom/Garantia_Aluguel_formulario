a103a6b07fd73d386e01097e1197c0f7381e68d3
55629 <55629@DESKTOP-4THO7TC>
2020-12-24 12:02:09 -0200

    upload apolice com email

--- uploadApolice.php
+++ uploadApolice.php
@@ -5,6 +5,13 @@
  
 require_once("php7_mysql_shim.php");
 
+function valida_email($email){
+    if(substr($email, -16) != "naoenviar.com.br")
+       return true;
+    else
+       return false;
+}
+
 $registro = $_REQUEST['codigo'];
 
 if ( !empty( $_FILES ) ) {
@@ -25,7 +32,31 @@ if ( !empty( $_FILES ) ) {
     $json = json_encode( $answer );
     //echo $json;
 
-    $mensagem = "<html><body><div align='center'><b>** Segue em anexo</body></html>";
+
+    //*************************PARTE DO ENVIO DE EMAIL DA APOLICE *************************//
+    $pdo = new PDO("mysql:host=localhost;dbname=segurosja;", "segurosja", "m1181s2081_", array());
+     
+      //$pdo = new PDO("mysql:host=localhost;dbname=segurosja;", "root", "", array());
+    
+    $sql = "SELECT *,(SELECT email_fianca FROM corretores WHERE corretores.codigo=fianca.corretor) as email_corretor1,
+            (SELECT email_fianca2 FROM corretores WHERE corretores.codigo=fianca.corretor) as email_corretor2,
+            (SELECT email_fianca3 FROM corretores WHERE corretores.codigo=fianca.corretor) as email_corretor3
+            from fianca where codigo='$registro'";
+    
+    $sth = $pdo->prepare($sql);
+    $sth->execute();
+    $dados_fianca = $sth->fetch(PDO::FETCH_ASSOC);
+
+$email_imob      = $dados_fianca['email_imobs'];
+$email_alt1      = $dados_fianca['email_alt1'];
+$email_alt2      = $dados_fianca['email_alt2'];
+$email_alt3      = $dados_fianca['email_alt3'];
+$email_cor1      = $dados_fianca['email_corretor1'];
+$email_cor2      = $dados_fianca['email_corretor2'];
+$email_cor3      = $dados_fianca['email_corretor3'];
+$cod_cor         = $dados_fianca['corretor'];
+
+$mensagem = "<html><body><div align='center'><b>Segue em anexo a Apólice</b></body></html>";
 
 // Inclui o arquivo class.phpmailer.php localizado na pasta phpmailer
 require("../../adm/phpmailer/class.phpmailer.php");
@@ -40,7 +71,7 @@ $mail->Password = 'm1181s2081_'; // Senha do servidor SMTP (senha do email usado
 // Define o remetente
 $mail->From = "cobertura@segurosja.com.br"; // Seu e-mail
 $mail->Sender = "cobertura@segurosja.com.br"; // Seu e-mail
-$mail->FromName = "Seguros Já! Cadastro"; // Seu nome
+$mail->FromName = "Seguros Já! Apólice"; // Seu nome
 $mail->AddReplyTo("cobertura@segurosja.com.br"); // Email para receber as respostas
 // Define os dados técnicos da Mensagem
 $mail->IsHTML(true); // Define que o e-mail será enviado como HTML
@@ -48,9 +79,27 @@ $mail->CharSet = 'iso-8859-1'; // Charset da mensagem (opcional)
 
 $mail->AddBCC("denilson.anhanguera@gmail.com");
 
-$mail->Body = $mensagem;//apagar
+if(valida_email($email_imob)){$mail->AddAddress("$email_imob");}
+if($email_cor1 <> ""){$mail->AddAddress("$email_cor1");}
+if($email_cor2 <> ""){$mail->AddCC("$email_cor2");}
+if($email_cor3 <> ""){$mail->AddCC("$email_cor3");}
+
+if(!empty(trim($email_alt1)) && (valida_email($email_alt1))){
+    $mail->AddAddress("$email_alt1");
+}
+        
+if(!empty(trim($email_alt2)) && (valida_email($email_alt2))){
+    $mail->AddAddress("$email_alt2");
+}
+        
+if(!empty(trim($email_alt3)) && (valida_email($email_alt3))){
+    $mail->AddAddress("$email_alt3");
+}
+
+//$mail->Body = $mensagem;//apagar
+$mail->MsgHTML($mensagem);
 $mail->Subject = "Apólice"; //apagar
-$mail->AddAttachment($tempPath, $arquivo_nome);// anexar arquivo
+$mail->AddAttachment($uploadPath);// anexar arquivo
 $enviado = $mail->Send();//apagar
 
 // Limpa os destinatários e os anexos
